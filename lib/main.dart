b5472aa9d283b88ca8d9e7910fcbe4a4fc5db91e
jack <darnelljack@icloud.com>
2023-01-18 23:38:04 -0500

    added scaffolding and text

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,19 @@
 //first flutter app written by jack darnell from course flutter complete guide
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my default text!'),
+      ), //scaffold creates a basic page design for app
+    );
+  }
+}
